7c9cf4c2fb53d7c8aac3bd9dd1844d36eedd1f74
kasaden <aden74000@gmail.com>
2026-04-07 16:28:27 +0200

    Add crontab.sh to run main.py every 2 minutes

---
+++ crontab.sh
@@ -0,0 +1,11 @@
+# Définir le chemin vers l'exécutable
+PYTHON=/usr/local/bin/python
+
+# Lancer une première fois immédiatement au démarrage du conteneur
+cd /pipeline/app && $PYTHON main.py
+
+# Enregistrer la tâche cron (toutes les deux minutes)
+echo "*/2 * * * * cd /pipeline/app && $PYTHON main.py >> /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
+
+# Démarrer cron en foreground (nécessaire pour Docker)
+exec cron -f
